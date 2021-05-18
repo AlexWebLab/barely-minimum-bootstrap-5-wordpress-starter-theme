0b6d97e20cea1d44afd5efa844ff0a3ff095af27
Alex <alessandroweb@gmail.com>
2021-05-18 11:26:04 +0800

    WIP

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const { src, dest } = require('gulp');
+// const babel = require('gulp-babel');
+// const uglify = require('gulp-uglify');
+// const concat = require('gulp-concat');
+// const sourcemaps = require('gulp-sourcemaps');
+
+// exports.default = function() {
+//   return src('bs5starter/js/*.js')
+//     .pipe(sourcemaps.init())
+// 		.pipe(babel({
+// 			presets: ['@babel/preset-env']
+// 		}))
+// 		.pipe(concat('scripts.min.js'))
+//     .pipe(uglify())
+// 		.pipe(sourcemaps.write('.'))
+//     .pipe(dest('bs5starter/js/'))
+// }
