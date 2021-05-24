24a61ae2f238f565b826bd2a026e0f9a4e3011e6
Alex <alessandroweb@gmail.com>
2021-05-24 10:39:33 +0800

    more recommended pattern

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,36 @@
 const gulp = require('gulp');
 const del = require('del');
 const sourcemaps = require('gulp-sourcemaps');
-// scripts imports
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
 // styles imports
 const sass = require('gulp-dart-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
+// scripts imports
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+// styles tasks
+function cleanStyles() {
+  return del([
+    'bs5starter/dist/css',
+  ]);
+};
+function buildStyles() {
+  return gulp.src('bs5starter/src/sass/styles.scss')
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(cleanCSS('level: 2'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('bs5starter/dist/css'))
+};
 // scripts tasks
-gulp.task('clean-scripts', () => {
+function cleanScripts() {
   return del([
     'bs5starter/dist/js',
   ]);
-});
-gulp.task('scripts', () => {
+};
+function buildScripts() {
   return gulp.src([
     'bs5starter/src/js/jquery.js',
     'bs5starter/src/js/bootstrap.js',
@@ -30,25 +44,12 @@ gulp.task('scripts', () => {
     .pipe(uglify())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('bs5starter/dist/js'));
-});
-// styles tasks
-gulp.task('clean-styles', () => {
-  return del([
-    'bs5starter/dist/css',
-  ]);
-});
-gulp.task('styles', function () {
-  return gulp.src('bs5starter/src/sass/styles.scss')
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(cleanCSS('level: 2'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('bs5starter/dist/css'))
-});
+};
 // watch tasks
-gulp.task('watch', function () {
-  gulp.watch(['bs5starter/src/js/*'], { ignoreInitial: false }, gulp.series('clean-scripts', 'scripts'));
-  gulp.watch(['bs5starter/src/sass/*'], { ignoreInitial: false }, gulp.series('clean-styles', 'styles'));
-});
-// default task
-gulp.task('default', gulp.series('watch'));
+function buildAndWatch() {
+  gulp.watch(['bs5starter/src/sass/*'], { ignoreInitial: false }, gulp.series(cleanStyles, buildStyles));
+  gulp.watch(['bs5starter/src/js/*'], { ignoreInitial: false }, gulp.series(cleanScripts, buildScripts));
+};
+// export tasks
+exports.build = gulp.parallel(gulp.series(cleanStyles, buildStyles), gulp.series(cleanScripts, buildScripts));
+exports.default = buildAndWatch;
