3f3e434fd94f3d1a5e90f34e8119ee12fac8c0b9
Alex <alessandroweb@gmail.com>
2021-05-24 15:10:59 +0800

    added paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+// paths
+const baseFolder = 'bs5starter/';
+const sourceFolder = baseFolder + 'src/';
+const distFolder = baseFolder + 'dist/';
 // global imports
 const gulp = require('gulp');
 const del = require('del');
@@ -14,46 +18,43 @@ const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 // styles tasks
 function cleanStyles() {
-  return del([
-    'bs5starter/dist/css',
-  ]);
+  return del([distFolder + 'css']);
 };
 function buildStyles() {
-  return gulp.src('bs5starter/src/sass/styles.scss')
+  return gulp.src(sourceFolder + 'sass/styles.scss')
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
     .pipe(autoprefixer('last 2 versions'))
-    .pipe(purgecss({ content: ['bs5starter/**/*.php', 'bs5starter/src/js/*.js'], variables: true }))
-    .pipe(gulp.dest('bs5starter/dist/css'))
+    .pipe(purgecss({
+      content: [baseFolder + '**/*.php', sourceFolder + 'js/*.js'],
+      variables: true
+    }))
+    .pipe(gulp.dest(distFolder + 'css'))
     .pipe(cleanCSS('level: 2'))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('bs5starter/dist/css'))
+    .pipe(gulp.dest(distFolder + 'css'))
 };
 // scripts tasks
 function cleanScripts() {
-  return del([
-    'bs5starter/dist/js',
-  ]);
+  return del([distFolder + 'js']);
 };
 function buildScripts() {
   return gulp.src([
-    'bs5starter/src/js/jquery.js',
-    'bs5starter/src/js/bootstrap.js',
+    sourceFolder + 'js/jquery.js',
+    sourceFolder + 'js/bootstrap.js',
   ])
     .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(babel({
-      presets: ['@babel/preset-env']
-    }))
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
     .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('bs5starter/dist/js'))
+    .pipe(gulp.dest(distFolder + 'js'))
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('bs5starter/dist/js'));
+    .pipe(gulp.dest(distFolder + 'js'));
 };
 // watch tasks
 function buildAndWatch() {
-  gulp.watch(['bs5starter/src/sass/*'], { ignoreInitial: false }, gulp.series(cleanStyles, buildStyles));
-  gulp.watch(['bs5starter/src/js/*'], { ignoreInitial: false }, gulp.series(cleanScripts, buildScripts));
+  gulp.watch([sourceFolder + 'sass/*'], { ignoreInitial: false }, gulp.series(cleanStyles, buildStyles));
+  gulp.watch([sourceFolder + 'js/*'], { ignoreInitial: false }, gulp.series(cleanScripts, buildScripts));
 };
 // export tasks
 exports.build = gulp.parallel(gulp.series(cleanStyles, buildStyles), gulp.series(cleanScripts, buildScripts));
