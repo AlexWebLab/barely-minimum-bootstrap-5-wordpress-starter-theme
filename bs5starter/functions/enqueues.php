cd6d9fbb738227008e0bfe9db44382ff5c972734
Alex <alessandroweb@gmail.com>
2021-08-26 09:26:50 +0800

    Updated Bootstrap to the latest v.5.1.0

--- bs5starter/functions/enqueues.php
+++ bs5starter/functions/enqueues.php
@@ -4,7 +4,7 @@ function custom_enqueue_scripts()
   // * styles *
 
   // Bootstrap 5 style
-  wp_register_style('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css', false, '5.0.0', null);
+  wp_register_style('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css', false, '5.1.0', null);
   wp_enqueue_style('bootstrap5');
 
   // custom style
@@ -19,7 +19,7 @@ function custom_enqueue_scripts()
   // wp_register_script('jquery', 'https://code.jquery.com/jquery-3.6.0.js', false, '3.6.0', false);
   // wp_enqueue_script('jquery');
 
-  wp_register_script('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js', false, '5.0.0', true);
+  wp_register_script('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js', false, '5.1.0', true);
   wp_enqueue_script('bootstrap5');
 
   // custom script
